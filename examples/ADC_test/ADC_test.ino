ff7115832ebcbe089fd8a2df99c28f2442c2d9bd
Souvik Saha <souvikssaha@gmail.com>
2020-01-15 11:48:54 +0530

    Made the example sketch easier to understand
    Added pin mapping, better example code

--- examples/ADC_test/ADC_test.ino
+++ examples/ADC_test/ADC_test.ino
@@ -9,10 +9,18 @@
 // Thanks to baturman for making this schematic.
 // https://easyeda.com/baturman/arduino-uno-r3-mcp3202-wiring-diagram
 
-MCP3202 adc = MCP3202(10);
+/**
+ *  This code shall return the voltage value in terms of a decimal value.
+ *  4096 means the voltage is equal to Vdd, while 0 means voltage is Vss.
+ */
 
-int channel1 = 0;
-int channel2 = 0;
+
+int Vdd = 5; //change according to your Vdd value.
+
+MCP3202 adc = MCP3202(10); //Parameter passed is the CS pin number. Change according to requirement.
+
+float channel1 = 0;
+float channel2 = 0;
 void setup() {
   // put your setup code here, to run once:
   adc.begin();
@@ -26,5 +34,14 @@ void loop() {
   Serial.print(channel1);
   Serial.print("\t");
   Serial.println(channel2);
+  
+  //To print voltage value, uncomment these lines
+  
+  //float voltage1 = (channel1*Vdd)/4096.0;
+  //float voltage2 = (channel2*Vdd)/4096.0;
+  //Serial.print(voltage1);
+  //Serial.print("\t");
+  //Serial.println(voltage2);
+  
   delay(100);
 }
